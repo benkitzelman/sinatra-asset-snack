8cac79b3b2426756ddda1a659ae02202fbf0c0de
Ben Kitzelman <benkitzelman@gmail.com>
2013-03-29 00:52:51 +1100

    Fixing load order in linux

--- lib/sinatra/asset_snack.rb
+++ lib/sinatra/asset_snack.rb
@@ -2,5 +2,8 @@ require 'sass'
 require 'coffee-script'
 
 here = File.dirname(__FILE__)
+
+require File.join here, 'asset_snack', 'compilers', 'asset_compiler'
+
 Dir[File.join(here, 'asset_snack', '*.rb')].each { |f| require f }
 Dir[File.join(here, 'asset_snack', 'compilers', '*.rb')].each { |f| require f }
