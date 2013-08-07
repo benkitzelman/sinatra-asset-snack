1dedf5b30540d50aadc4c34e9072ef152fe372d0
Keyvan Fatehi <keyvanfatehi@gmail.com>
2013-08-07 00:33:05 -0700

    Extend anonymous module instead of AssetSnack itself; also be more explicit about it to appease Travis

--- lib/sinatra/asset_snack/precompile.rb
+++ lib/sinatra/asset_snack/precompile.rb
@@ -2,14 +2,16 @@ require 'fileutils'
 
 module Sinatra
   module AssetSnack
-    extend InstanceMethods
-
     def self.precompile!
-      self.assets.each do |assets|
-        path = File.join(self.app.public_dir, assets[:route])
-        FileUtils.mkdir_p File.dirname(path)
-        File.open(path, 'w') do |file|
-          file.write compile(assets[:paths])[:body]
+      snack = self
+      Module.new do
+        extend snack::InstanceMethods
+        snack.assets.each do |assets|
+          path = File.join(snack.app.public_dir, assets[:route])
+          FileUtils.mkdir_p File.dirname(path)
+          File.open(path, 'w') do |file|
+            file.write compile(assets[:paths])[:body]
+          end
         end
       end
     end
